1149b07bfd9ae78c19ce7da139d1edc937d0465a
AzaanUllah-Khan <azaanullahkhan291@gmail.com>
2025-08-06 00:11:29 +0500

    delete functionality added

--- src/Components/TodoItems.jsx
+++ src/Components/TodoItems.jsx
@@ -1,15 +1,21 @@
 import { TrashIcon } from "@heroicons/react/24/outline"
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
+import {delTodo} from "../Features/todoSlice"
 
 const TodoItems = () => {
-    const todos = useSelector(state=>state.todos)
-
+    const todos = useSelector(state => state.todos)
+    const dispatch = useDispatch()
+    const deleteTodo = (id) => {
+        dispatch(delTodo(id))
+    }
     return (
         <div className="mt-5 flex flex-col w-full justify-center intems-center gap-y-3">
-            <div className="w-full inset-shadow-sm bg-gray-200/70 rounded-lg flex justify-start gap-4">
-                <button className="hover:bg-red-400/90 transition ease-in-out cursor-pointer bg-red-400 rounded-s-lg py-2 px-3 text-white"><TrashIcon className="size-5" /></button>
-                <h1 className="self-center text-gray-600 font-semibold">Azaan</h1>
-            </div>
+            {todos.map((todo) => {
+                return <div key={todo.id} className="w-full inset-shadow-sm bg-gray-200/70 rounded-lg flex justify-start gap-4">
+                    <button onClick={() => deleteTodo(todo.id)} className="hover:bg-red-400/90 transition ease-in-out cursor-pointer bg-red-400 rounded-s-lg py-2 px-3 text-white"><TrashIcon className="size-5" /></button>
+                    <h1 className="self-center text-gray-600 font-semibold">{todo.text}</h1>
+                </div>
+            })}
         </div>
     )
 }
