15bd71c9180cf7e0423dc2bcb8636e319dd921a8
AzaanUllah-Khan <azaanullahkhan291@gmail.com>
2025-08-05 23:35:25 +0500

    input add functionality added

--- src/Components/TodoInput.jsx
+++ src/Components/TodoInput.jsx
@@ -1,9 +1,21 @@
+import { useState } from "react"
+import { addTodo } from "../Features/todoSlice"
+import { useDispatch } from "react-redux"
+
 const TodoInput = () => {
+  const [input, setInput] = useState("")
+  const dispatch = useDispatch()
+  const add = (e) => {
+    e.preventDefault()
+    dispatch(addTodo(input))
+    setInput("")
+  }
+
   return (
-    <div className="inset-shadow-sm w-full bg-gray-200/70 rounded-lg flex justify-between">
-      <input type="text" className="text-sm w-full px-4 py-3 outline-none"/>
-      <button className="inset-shadow-sm text-sm py-3 px-3 text-center bg-green-400 cursor-pointer hover:bg-green-400/70 transition ease-in-out duration-200 font-semibold text-white rounded-r-lg">Add</button>
-    </div>
+    <form onSubmit={add} className="inset-shadow-sm w-full bg-gray-200/70 rounded-lg flex justify-between">
+      <input value={input} onChange={(e) => setInput(e.target.value)} placeholder="Type you todo here" type="text" className="text-sm w-full px-4 py-3 outline-none text-gray-800 placeholder:text-gray-400" />
+      <button type="submit" className="inset-shadow-sm text-sm py-3 px-3 text-center bg-green-400 cursor-pointer hover:bg-green-400/70 transition ease-in-out duration-200 font-semibold text-white rounded-r-lg">Add</button>
+    </form>
   )
 }
 
