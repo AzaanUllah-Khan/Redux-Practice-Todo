80fe20cc66fe49f71c66858ffabc9654f6f7166e
AzaanUllah-Khan <azaanullahkhan291@gmail.com>
2025-08-05 23:22:38 +0500

    Input form styled and added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import TodoInput from "./Components/TodoInput"
+
 const App = () => {
   return (
-    <div className="flex justify-center items-center w-full min-h-screen bg-gray-200/70">
-      <div className="bg-white shadow-md shadow-gray-200/50 p-5 rounded-lg"> </div>
+    <div className="p-5 flex justify-center items-center w-full min-h-screen bg-gray-200/70">
+      <div className="w-full max-w-xl bg-white shadow-md shadow-gray-200/50 p-5 rounded-lg">
+      <TodoInput/>
+      </div>
     </div>
   )
 }
--- src/Components/TodoInput.jsx
+++ src/Components/TodoInput.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
-
-const InputTodo = () => {
+const TodoInput = () => {
   return (
-    <div>
-      
+    <div className="inset-shadow-sm w-full bg-gray-200/70 rounded-lg flex justify-between">
+      <input type="text" className="text-sm w-full px-4 py-3 outline-none"/>
+      <button className="inset-shadow-sm text-sm py-3 px-3 text-center bg-green-400 cursor-pointer hover:bg-green-400/70 transition ease-in-out duration-200 font-semibold text-white rounded-r-lg">Add</button>
     </div>
   )
 }
 
-export default InputTodo
+export default TodoInput
