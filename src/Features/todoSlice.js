6cbca085c0ab13fe9bd059f254110018f3e69f09
AzaanUllah-Khan <azaanullahkhan291@gmail.com>
2025-08-05 23:01:55 +0500

    Reux Set Up completed

---
+++ src/Features/todoSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
+    todos:[]
+}
+
+export const todoSlice = createSlice({
+    name: "todos",
+    initialState,
+    reducers:{
+        addTodo: (state,action)=>{
+            state.todos.push(action.payload)
+        },
+        delTodo: (state,action)=>{
+            state.todos = state.todos.filter((prev)=>prev.id !== action.payload)
+        }
+    }
+})
+
+export const {addTodo,delTodo} = todoSlice.actions
+
+export default todoSlice.reducer
